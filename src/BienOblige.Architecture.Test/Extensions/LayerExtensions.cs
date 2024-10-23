6eb2da36cd01b6aa8dcb3c18d73f81ddec4694e4
Barry Stahl <bss_github@bsstahl.com>
2024-10-23 06:59:50 -0700

    Add ArchUnit support. At this point, I haven't gotten the tests working, but I've spent too much time on it right now. It is in a good place (test failing) where it should be easy to pick back up.

---
+++ src/BienOblige.Architecture.Test/Extensions/LayerExtensions.cs
@@ -0,0 +1,11 @@
+﻿using ArchUnitNET.Fluent;
+using BienOblige.Architecture.Test.Enumerations;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace BienOblige.Architecture.Test.Extensions;
+
+internal static class LayerExtensions
+{
+    internal static IArchRule ShouldNotDependUpon(this Layer layer, Layer otherLayer) =>
+        Types().That().Are(layer.ToString()).Should().NotDependOnAny(otherLayer.ToString());
+}
